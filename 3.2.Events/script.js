3530a5dce1f10ba15230ccd3834fc0bef7446b2a
Virginie Dourson <virginiedourson@gmail.com>
2022-12-05 16:35:56 +0100

    Events point 2

--- 3.2.Events/script.js
+++ 3.2.Events/script.js
@@ -10,9 +10,12 @@ const liste = document.querySelector("ul");
 const clickOnSquare = (e) => {
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+  //créer une nouvelle div carrée de la même couleur que celle du
+  //carré sur lequel on a cliqué
   const newSquare = document.createElement("div");
   newSquare.className = "displayedsquare " + e.target.classList[1];
   sections[0].appendChild(newSquare);
+  //Ajouter l'évènement dans la liste des log
   const newLiLog = document.createElement("li");
   const newLog = document.createTextNode(getElapsedTime() + " Created a new " + e.target.classList[1] + " square");
   liste.appendChild(newLiLog);
@@ -23,6 +26,41 @@ const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare);
 }
+
+const evenem = (ev) => {
+  //Quand la touche spacebar est enfoncée
+  if (ev.key == " "){
+    //Générer les paramètres pour une couleur aléatoire et
+    //les appliquer au fond du document 
+    let r= Math.floor(Math.random() * 256);
+    let g= Math.floor(Math.random() * 256);
+    let b= Math.floor(Math.random() * 256);
+    document.body.style.backgroundColor =`rgb(${r},${g},${b})`;
+    //Ajouter l'évènement dans la liste des log
+    const newLiLog = document.createElement("li");
+    const newLog = document.createTextNode(getElapsedTime() + " spacebar used");
+    liste.appendChild(newLiLog);
+    newLiLog.appendChild(newLog);
+  }
+  //Quand la touche I est enfoncée
+  if (ev.key == "I"){
+    while (liste.children.length >0){
+      liste.removeChild(liste.lastElementChild);
+    }
+  }
+  //Quand la touche S est enfoncée
+  if (ev.key == "s"){
+    while (sections[0].children.length>0){
+      sections[0].removeChild(sections[0].lastElementChild);
+    }
+  }
+  console.log(ev.code);
+}
+
+document.body.addEventListener('keydown',evenem);
+
+
+
 
 /*Lors de l'évènement "cliquer sur le carré vert",
 //création d'une div de classes "displayedsquare" et "green"
